95829cf787646d93f31286cfbad94e3be8650944
sudhanshu2 <24910339+sudhanshu2@users.noreply.github.com>
2026-08-28 15:55:31 -0500

    configs: Add parallel X86 Ubuntu run script

---
+++ configs/example/gem5_library/x86-ubuntu-run-parallel.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2026 Sudhanshu Agarwal
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+This script uses X86DemoParallelBoard to boot Ubuntu before exiting.
+
+Usage
+-----
+
+```
+scons build/X86/gem5.opt
+./build/X86/gem5.opt configs/example/gem5_library/x86-ubuntu-run-parallel.py
+```
+"""
+
+from gem5.prebuilt.demo.x86_demo_parallel_board import X86DemoParallelBoard
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.simulator import Simulator
+
+board = X86DemoParallelBoard()
+
+workload = obtain_resource(
+    "x86-ubuntu-24.04-boot-with-systemd", resource_version="5.0.0"
+)
+board.set_workload(workload)
+
+simulator = Simulator(board=board)
+
+simulator.run()
